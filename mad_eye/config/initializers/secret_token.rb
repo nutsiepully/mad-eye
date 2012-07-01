deebb68aadce7d422a45e57a11ff6c7233ca6839
pulkit <pulkit.bosco05@gmail.com>
2012-07-02 01:04:51 +0530

    pulkit | Adding in the rails app, changes made by biplav for taag

---
+++ mad_eye/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadEye::Application.config.secret_token = '4791d65d1f9b729c9f3f7b1ba10f3ecfe9081583a89855fc89b438c6472da8e0fa5166eca95527c82c85c52ca59d847731c822acc646c3fe97afd64b39cd1078'
--- server/curls/factories.rb
+++ server/curls/factories.rb
@@ -1,4 +1,5 @@
 #factories.rb
+require 'nokogiri'
 
 module AirlineRequestor
   def request(source,destination,start_date,return_date)
@@ -8,25 +9,38 @@ end
 
 def cli(url,file)
   puts "#{url}"
+  user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.43 Safari/536.11"
+  header_language="Accept-Language: en-US,en;q=0.8"
+  header_charset="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3"
+
   `rm cookie.file`
-  `curl --dump-header #{file}_header --cookie cookie.file --cookie-jar cookie.file --location #{url} > #{file}`
+  `curl --dump-header #{file}_header --cookie cookie.file --cookie-jar cookie.file --header '#{header_language}' --header '#{header_charset}' --user-agent '#{user_agent}' --location '#{url}' > #{file}`
 end
 
 class TaagAirlineRequestor
   include AirlineRequestor
+  file_name = "taag.req"
+
   $travel_class_map = {
     "economy" => "Economy",
     "business" => "Business"
   }
+
   def request(source,destination,start_date,return_date,travel_class)
   #dateFormat 29/06/2012
     url="https://taag.sita.aero/itd/itd/DoAirSearch?_destinationSelected=Airport.#{destination}&_originSelected=Airport.#{source}&_adults=1&_children=0&_infants=0&_depdateeu=#{start_date}&_retdateeu=#{return_date}&_classType=#{travel_class}&requestor=AirSimpleReqs&_channelLocale=en"
-    taag="taag.req"
-    cli(url,taag)
+    cli(url,file_name)
   end
+
+  def parse
+  	doc = Nokogiri::HTML(open(file_name))
+	puts doc.css("td data block matrix headlineonewayprice fmt1")
+		
+  end 
 end  
 
-TaagAirlineRequestor.new.request("LIS","LAD","29/06/2012","01/07/2012","Economy")
+#TaagAirlineRequestor.new.request("LAD","LIS","02/07/2012","09/07/2012","Economy")
+TaagAirlineRequestor.new.parse
 puts "Done!"
 
     
