fdd87b977c263a3bd50c20bd9a310dcaf2ba66c0
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2012-08-09 19:38:07 +0000

    pulkit | modifying kenya_try.rb

--- kenya_try.rb
+++ kenya_try.rb
@@ -8,21 +8,34 @@ browser = Watir::Browser.new
 
 browser.goto "http://www.kenya-airways.com/default_en.aspx"
 
-browser.radio(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_rblBooking_0').set
-#browser.radio(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_rblBooking_1').set
+one_way = true
+
+if !one_way
+	browser.radio(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_rblBooking_0').set
+else
+	browser.radio(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_rblBooking_1').set
+	sleep(30)
+end
 
 browser.select(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_lstFrom').select_value('LAD')
 browser.select(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_lstTo').select_value('DXB')
 
-browser.text_field(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_txtDepartureDate').set('01-08-2012')
-browser.text_field(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_txtReturnDate').set('11-08-2012')
+browser.text_field(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_txtDepartureDate').set('04-09-2012')
+if (!one_way) 
+	browser.text_field(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_txtReturnDate').set('11-09-2012')
+end
 
-browser.select(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_lstCabinClass').select_value('B')
+browser.select(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_lstCabinClass').select_value('E')
 
 browser.button(:id => 'ctl00_cph_flightsearch_pagekqbookingengine_cmdGo').click
 
-browser.frame().wait_until_present(180)
-puts browser.frame().td(:id => 'cell_3_3').text
+browser.frame.wait_until_present
+
+if !one_way
+	puts browser.frame.td(:id => 'cell_3_3').text
+else
+	puts browser.frame.td(:id => 'cell_3_0').text
+end
 
 browser.close
 headless.destroy
