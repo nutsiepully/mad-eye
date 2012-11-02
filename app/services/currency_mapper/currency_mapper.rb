4040d15668cc24fc460310bc5f8a16e186d83d04
Pulkit <pulkit.bosco05@gmail.com>
2012-11-03 00:11:50 +0530

    pulkit | Fixing emirates issue. new currency mapper with app_id

--- app/services/currency_mapper/currency_mapper.rb
+++ app/services/currency_mapper/currency_mapper.rb
@@ -8,7 +8,7 @@ module CurrencyMapper
 
     def self.get_currency_map
       return @@currency_map unless @@currency_map.blank?
-      `curl http://openexchangerates.org/latest.json > currency_rates`
+      `curl http://openexchangerates.org/api/latest.json?app_id=c9db0dfae0284f598062c299fd843233 > currency_rates`
       currency_json = File.read 'currency_rates'
       @@currency_map = JSON.parse(currency_json)['rates']
     end
